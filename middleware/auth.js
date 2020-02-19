c3df7014ae3fe831369fc1e318959f2948491180
Edgar Jose Romero <edgarr@thehangar.cr>
2020-02-18 19:01:22 -0600

    agregando Access-Control-Allow-Origin

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = function(req, res, next) {
+    
     // Leer el token del header
     const token = req.header('x-auth-token');
 
@@ -13,6 +14,7 @@ module.exports = function(req, res, next) {
     try {
         const cifrado = jwt.verify(token, process.env.SECRETA);
         req.usuario = cifrado.usuario;
+        res.header('Access-Control-Allow-Origin', '*');
         next();
     } catch (error) {
         res.status(401).json({msg: 'Token no válido'})
